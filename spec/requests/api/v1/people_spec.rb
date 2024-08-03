87734b23b7b93a8fa8026c5418b2ce72ec606334
Emad Rahimi <121079771+ersync@users.noreply.github.com>
2024-08-03 09:58:36 +0330

    Add request spec for PeopleController

--- spec/requests/api/v1/people_spec.rb
+++ spec/requests/api/v1/people_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::People", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  # describe "GET /index" do
+  #   pending "add some examples (or delete) #{__FILE__}"
+  # end
+  describe "Get /api/v1/generate" do
+    it "returns a JSON with fake peron data" do
+      get "/api/v1/generate"
+      expect(response).to have_http_status(:ok)
+      json_response = JSON.parse(response.body)
+      expect(json_response['data']).to include(
+                                        "name", "email",
+                                        "address", "phone",
+                                        "date_of_birth"
+                                      )
+    end
   end
 end
