04039752da5178a090b0d953ac9464c09019842a
Emad Rahimi <121079771+ersync@users.noreply.github.com>
2024-08-05 08:13:02 +0330

    Apply RuboCop auto-corrections for code style consistency

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::PeopleController < ApplicationController
   def generate
     person = {
       name: Faker::Name.name,
-      email:Faker::Internet.email,
-      address:Faker::Address.full_address,
-      phone:Faker::PhoneNumber.phone_number,
-      date_of_birth: Faker::Date.birthday(min_age:18,max_age:65),
+      email: Faker::Internet.email,
+      address: Faker::Address.full_address,
+      phone: Faker::PhoneNumber.phone_number,
+      date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 65)
     }
-    render json: {data: person}, status: :ok
+    render json: { data: person }, status: :ok
   end
 end
--- spec/requests/api/v1/people_spec.rb
+++ spec/requests/api/v1/people_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::People", type: :request do
+RSpec.describe 'Api::V1::People', type: :request do
   # describe "GET /index" do
   #   pending "add some examples (or delete) #{__FILE__}"
   # end
-  describe "Get /api/v1/generate" do
-    it "returns a JSON with fake peron data" do
-      get "/api/v1/generate"
+  describe 'Get /api/v1/generate' do
+    it 'returns a JSON with fake peron data' do
+      get '/api/v1/generate'
       expect(response).to have_http_status(:ok)
-      json_response = JSON.parse(response.body)
+      json_response = response.parsed_body
       expect(json_response['data']).to include(
-                                        "name", "email",
-                                        "address", "phone",
-                                        "date_of_birth"
-                                      )
+        'name', 'email',
+        'address', 'phone',
+        'date_of_birth'
+      )
     end
   end
 end
